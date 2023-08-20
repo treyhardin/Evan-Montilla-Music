ee40cde92b248b18fc4f1f32ac9482d91c8664d0
Trey Hardin <treyhardindesign@gmail.com>
2023-08-20 14:32:47 -0400

    Fallback Hero Img

--- src/components/AlienVideo/AlienVideo.jsx
+++ src/components/AlienVideo/AlienVideo.jsx
@@ -1,5 +1,6 @@
 import webm from '../../assets/alienVideo.webm'
 import hevc from '../../assets/alienVideo.mov'
+import fallbackImage from '../../assets/alienVideoFallback.png'
 import styles from './AlienVideo.module.css'
 import { createSignal, onMount } from 'solid-js'
 
@@ -18,12 +19,13 @@ export default function AlienVideo() {
 
   return (
     <>
-      {/* <video width="1920" height="1920" autoplay loop muted playsinline class={styles.video}>
+      <video width="1920" height="1920" poster={fallbackImage} autoplay loop muted playsinline class={styles.video}>
         <source src={webm} type="video/webm" />
         <source src={hevc} type='video/mp4; codecs="hvc1"' />
-        Your browser doesn't support this alien technology.
-      </video> */}
-      <video ref={videoPlayer} width="1920" height="1920" autoplay loop muted playsinline class={styles.video} src={webm} />
+        <source src={hevc} type='video/mp4' />
+        {/* <img src={fallbackImage} width="1920" height="1920" title="Your browser doesn't support this alien video technology."></img> */}
+      </video>
+      {/* <video ref={videoPlayer} width="1920" height="1920" autoplay loop muted playsinline class={styles.video} src={webm} /> */}
     </>
   )
 }
